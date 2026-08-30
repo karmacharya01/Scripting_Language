d3ae190a09bd971cc1cd8fc3714ef9ba6b895b07
karmacharya01 <karmacharyakritish24@gmail.com>
2026-08-30 17:50:23 +0545

    javascript

---
+++ OperatorsInjavascript.js
@@ -0,0 +1,51 @@
+// JavaScript Program Using Almost All Operators
+let a = 10;
+let b = 5;
+
+console.log(`a = ${a}\tb = ${b}`);
+console.log();
+
+// Arithmetic Operators
+console.log("Arithmetic Operators");
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
+console.log("Exponent:", a ** b);
+console.log();
+
+// Assignment Operators
+console.log("Assignment Operators");
+console.log((c = 15));
+console.log((a += b));
+console.log((a -= b));
+console.log((a *= b));
+console.log((a /= b));
+console.log((a %= b));
+console.log();
+
+// Comparison Operators
+console.log("Comparision Operators");
+
+console.log("a > b:", a > b);
+console.log("a < b:", a < b);
+console.log("a >= b:", a >= b);
+console.log("a <= b:", a <= b);
+console.log();
+
+// Logical Operators
+console.log("logical Operators");
+
+let x = true;
+let y = false;
+
+console.log("x && y:", x && y);
+console.log("x || y:", x || y);
+console.log("!x:", !x);
+console.log();
+
+// Ternary Operator
+console.log("Ternary Operators");
+let result = a > b ? "a is greater" : "b is greater";
+console.log(result);
